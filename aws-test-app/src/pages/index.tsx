e809dc44646db3c88571e0182c4fdf3b9667b3ef
Ritigahapola Dennis <d508485@VAL-027126.local>
2025-09-05 20:05:14 +0200

    new pipeline test

--- aws-test-app/src/pages/index.tsx
+++ aws-test-app/src/pages/index.tsx
@@ -4,8 +4,8 @@ export default function Home() {
       <div className="flex flex-1 flex-col items-center justify-center w-full px-20 text-center">
         <h1 className="text-4xl font-bold">Dennis AWS Journey</h1>
         <h3 className="text-xl">
-          I got no idea what to put on this website so I decided to just write up
-          my journey of what I learned
+          I got no idea what to put on this website so I decided to just write
+          up my journey of what I learned
         </h3>
       </div>
       <div className="flex flex-col flex-7 w-full justify-start items-start">
@@ -130,35 +130,67 @@ export default function Home() {
             How would access be restricted in a production setting?
           </li>
         </ul>
-         <h3 className="text-2xl font-bold mb-1 mt-2">
+        <h3 className="text-2xl font-bold mb-1 mt-2">
           3. Setting up a Github actions pipeline to create new builds 5.9
         </h3>
         <p className="ml-3">
-          So next up was setting up a pipeline. Why you ask? No clue it sounded cool thats why.
-        </p>
-          <p className="ml-3">
-          What are Github action runners? They are VMs that run the command set in a yaml file in the .github/workflows folder
-        </p>
-        <p className="ml-3">From my understanding they can basically do anything you want them to do as long as you know the commands. So we set up a simple pipeline. It should use the keys to ssh into the instance and then pull the changes on the main branch, install the dependencies and run a new build. Simple enough</p>
-        <p className="ml-3">
-          Set up the environment with the host name, the ip and a private key. Created a key pair. Private key goes to github secrets, public key goes to the instance. This way the VM can authenticate itself when trying to ssh into the instance.
+          So next up was setting up a pipeline. Why you ask? No clue it sounded
+          cool thats why.
         </p>
         <p className="ml-3">
-          Now it worked it got the right secrets to access. But the Instance refused to connect. Think back to the inbound security rules. The instance refuses connections which arent from my connection. The github runner has a different ip. So i changed the inbound rule temporarly to allow all connections.
+          What are Github action runners? They are VMs that run the command set
+          in a yaml file in the .github/workflows folder
         </p>
         <p className="ml-3">
-          Now it ran but ran it but the npm install failed? It was at the wrong directory. The github actions runner was in the root directory which is not where the project is. So added a new command to cd into the project.
+          From my understanding they can basically do anything you want them to
+          do as long as you know the commands. So we set up a simple pipeline.
+          It should use the keys to ssh into the instance and then pull the
+          changes on the main branch, install the dependencies and run a new
+          build. Simple enough
         </p>
         <p className="ml-3">
-          Ran it again and now it worked. But the page didnt change? Why? Because I forgot to actually build the project. So added a new command to run the build script. Now it should work
+          Set up the environment with the host name, the ip and a private key.
+          Created a key pair. Private key goes to github secrets, public key
+          goes to the instance. This way the VM can authenticate itself when
+          trying to ssh into the instance.
         </p>
         <p className="ml-3">
-          And it worked. After what felt like 100 github runners running for their dear life and failing it worked. Now still need to resolve the issue that i cant run it because if the inbound rules. Fixing it would require my workflow changing the permissions then running then setting them back.
+          Now it worked it got the right secrets to access. But the Instance
+          refused to connect. Think back to the inbound security rules. The
+          instance refuses connections which arent from my connection. The
+          github runner has a different ip. So i changed the inbound rule
+          temporarly to allow all connections.
         </p>
         <p className="ml-3">
-          And thats what I did. But it didnt work. I couldnt change the permissions to get the ID and Key as I dont have them. And I couldnt create a new user as i dont have the permissions to do so. So this chapter ends here for now.
+          Now it ran but ran it but the npm install failed? It was at the wrong
+          directory. The github actions runner was in the root directory which
+          is not where the project is. So added a new command to cd into the
+          project.
         </p>
-        </div>
+        <p className="ml-3">
+          Ran it again and now it worked. But the page didnt change? Why?
+          Because I forgot to actually build the project. So added a new command
+          to run the build script. Now it should work
+        </p>
+        <p className="ml-3">
+          And it worked. After what felt like 100 github runners running for
+          their dear life and failing it worked. Now still need to resolve the
+          issue that i cant run it because if the inbound rules. Fixing it would
+          require my workflow changing the permissions then running then setting
+          them back.
+        </p>
+        <p className="ml-3">
+          And thats what I did. But it didnt work. I couldnt change the
+          permissions to get the ID and Key as I dont have them. And I couldnt
+          create a new user as i dont have the permissions to do so. So this
+          chapter ends here for now.
+        </p>
+        <p className="ml-3">
+          Roles vs users. A user is an individual identity with specific
+          permissions, while a role is a set of permissions that can be assumed by
+          users or services to perform specific tasks without needing permanent
+        </p>
+      </div>
     </div>
   );
 }
