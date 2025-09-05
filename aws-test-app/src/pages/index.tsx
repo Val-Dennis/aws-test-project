8b1a4b21c6a117a200b0c9f6720f3dddfb397676
Ritigahapola Dennis <d508485@VAL-027126.local>
2025-09-05 15:52:58 +0200

    update

--- aws-test-app/src/pages/index.tsx
+++ aws-test-app/src/pages/index.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen min-w-screen">
-      <p>Hallo</p>
+    <div className="flex flex-col items-center justify-start min-h-screen min-w-screen">
+      <div className="flex flex-1 flex-col items-center justify-center w-full px-20 text-center">
+      <h1 className="text-4xl font-bold">Dennis AWS Journey</h1>
+      <h3 className="text-xl">As I got no idea what to put on this website I decided to just write up my journey of what I learned</h3>
+      </div>
+      <div className="flex flex-col flex-7 w-full justify-start items-center">
+        <h2 className="text-2xl font-bold">Chapter 1</h2>
+      </div>
     </div>
   );
 }
